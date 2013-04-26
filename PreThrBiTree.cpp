1480b0ac0d3a32274561ea682476324d0121cbb9
sumy <sunmingjian8@gmail.com>
2013-04-26 15:35:05 +0800

    前序线索二叉树
    前序建树，线索化，遍历，释放

---
+++ PreThrBiTree.cpp
@@ -0,0 +1,142 @@
+/*************************************************
+Copyright:哪有神马版权呀(╯‵□′)╯︵┻━┻
+Author:sumy
+Date:2013-04-26
+Description:前序线索二叉树示例
+**************************************************/
+
+#include <iostream>
+using namespace std;
+
+template<class T> class PreThrBiTree;
+template<class T>
+class ThrNode
+{
+ private:
+    T data;
+    ThrNode *lchild;
+    ThrNode *rchild;
+    int ltag,rtag;
+ public:
+    friend class PreThrBiTree<T>;
+    ThrNode()
+    {
+        lchild=NULL;
+        rchild=NULL;
+        ltag=0;
+        rtag=0;
+    }
+};
+
+template<class T>
+class PreThrBiTree
+{
+public:
+    PreThrBiTree()
+    {
+        root=Creat(root);
+        pre=NULL;
+        ThrBiTree(root);
+    }
+    ~PreThrBiTree()
+    {
+        Release(root);
+    }
+    ThrNode<T>* Next(ThrNode<T> *p);
+    void PreOrder();
+private:
+    ThrNode<T> *root;
+    ThrNode<T> *Creat(ThrNode<T> *bt);
+    void Release(ThrNode<T> *bt);
+    ThrNode<T> *pre;
+    void ThrBiTree(ThrNode<T> *bt);
+};
+
+template<class T>
+ThrNode<T> *PreThrBiTree<T>::Creat(ThrNode<T> *bt)
+{
+    char ch;
+    cin>>ch;
+    if(ch=='#') bt=NULL;
+    else
+    {
+        bt=new ThrNode<T>;
+        bt->data=ch;
+        bt->ltag=0;
+        bt->rtag=0;
+        bt->lchild=Creat(bt->lchild);
+        bt->rchild=Creat(bt->rchild);
+    }
+    return bt;
+}
+
+template<class T>
+void PreThrBiTree<T>::Release(ThrNode<T> *bt)
+{
+    if(bt!=NULL)
+    {
+        if(bt->ltag==0) Release(bt->lchild);
+        if(bt->rtag==0) Release(bt->rchild);
+        delete bt;
+    }
+}
+
+template<class T>
+void PreThrBiTree<T>::ThrBiTree(ThrNode<T> *bt)
+{
+    if(bt==NULL) return;
+    if(bt->lchild==NULL)
+    {
+        bt->ltag=1;
+        bt->lchild=pre;
+    }
+    if(bt->rchild==NULL) bt->rtag=1;
+    if(pre!=NULL)
+        if(pre->rtag==1) pre->rchild=bt;
+    pre=bt;
+    //cout<<bt->data<<endl;
+    if(bt->ltag==0) ThrBiTree(bt->lchild);
+    if(bt->rtag==0) ThrBiTree(bt->rchild);
+}
+
+template<class T>
+ThrNode<T> *PreThrBiTree<T>::Next(ThrNode<T> *p)
+{
+    ThrNode<T> *q;
+    if(p->rtag==1) q=p->rchild;
+    else
+    {
+        if(p->ltag==0) q=p->lchild;
+        else q=p->rchild;
+    }
+    return q;
+}
+
+template<class T>
+void PreThrBiTree<T>::PreOrder()
+{
+    ThrNode<T> *q;
+    if(root==NULL) return;
+    q=root;
+    cout<<q->data;
+    while(q->rchild!=NULL)
+    {
+        q=Next(q);
+        cout<<q->data;
+    }
+}
+
+int main()
+{
+    PreThrBiTree<char> tre=PreThrBiTree<char>();
+    tre.PreOrder();
+    cout<<endl;
+    return 0;
+}
+
+/*
+
+AB#CE##D######
+AB#D##C##
+
+*/
